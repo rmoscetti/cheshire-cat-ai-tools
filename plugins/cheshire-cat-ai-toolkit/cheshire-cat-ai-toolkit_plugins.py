5761f112715a1fb4393153d21b95d4c5f5141d1f
Roberto Moscetti <rmoscetti@unitus.it>
2025-01-11 21:29:55 +0100

    Update cheshire-cat-ai-toolkit_plugins.py

--- plugins/cheshire-cat-ai-toolkit/cheshire-cat-ai-toolkit_plugins.py
+++ plugins/cheshire-cat-ai-toolkit/cheshire-cat-ai-toolkit_plugins.py
@@ -2,29 +2,28 @@ import numpy as np
 import json
 from cat.mad_hatter.decorators import tool
 
-@tool #(return_direct=True)
-def zero_order_equation(tool_input, cat):
+@tool (return_direct=False, examples=["linear equation", "give me the results of a linear equation", "provide the solution to the linear equation", "compute a linear equation"])
+def linear_equation(tool_input, cat):
     """
-    Provide only the structure of the tool_input as follow. No additional comments.
+    Essential to compute the result (y) of a linear equation (y = a*x + b).
 
-    Args:
-        tool_input: A JSON string with the following structure:
-            {
-                "x": [x1, x2, x3, ...], 
-                "a": <float>, 
-                "b": <float>
-            }
-            where:
-                - x: A list of float values representing the independent variable.
-                - a: The slope of the linear equation (float).
-                - b: The intercept of the linear equation (float).
+    Input is always a JSON string structures follows:
+        {
+            "x": [x1, x2, x3, ...], 
+            "a": <float>, 
+            "b": <float>
+        }
+        where:
+            - x: A list of float values representing the independent variable.
+            - a: The slope of the linear equation (float).
+            - b: The intercept of the linear equation (float).
 
     Returns:
         A JSON string containing the following keys:
             - x: List of float values for the independent variable.
             - slope: Value of the slope (float).
             - intercept: Value of the intercept (float).
-            - y: List of float values for the dependent variable calculated using the zero-order equation (y = a*x + b).
+            - y: List of float values for the dependent variable calculated using the linear equation
     """
 
     data = json.loads(tool_input) 
@@ -43,4 +42,4 @@ def zero_order_equation(tool_input, cat):
         "y": y.tolist()
     }
 
-    return str("Use the following JSON data in your response: " + json.dumps(output))
+    return str(json.dumps(output))
