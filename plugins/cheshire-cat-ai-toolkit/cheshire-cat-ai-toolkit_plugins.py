095475ac1fbf2aac2e9ae60fad2ee6a4d43eb3f1
Simone Massaro <simone.massaro@mone27.net>
2025-01-12 00:55:43 +0100

    ✨ added time_to_finish tool in plugin

--- plugins/cheshire-cat-ai-toolkit/cheshire-cat-ai-toolkit_plugins.py
+++ plugins/cheshire-cat-ai-toolkit/cheshire-cat-ai-toolkit_plugins.py
@@ -2,7 +2,8 @@ import numpy as np
 import json
 from cat.mad_hatter.decorators import tool
 
-@tool #(return_direct=True)
+
+@tool  # (return_direct=True)
 def zero_order_equation(tool_input, cat):
     """
     Provide only the structure of the tool_input as follow. No additional comments.
@@ -10,8 +11,8 @@ def zero_order_equation(tool_input, cat):
     Args:
         tool_input: A JSON string with the following structure:
             {
-                "x": [x1, x2, x3, ...], 
-                "a": <float>, 
+                "x": [x1, x2, x3, ...],
+                "a": <float>,
                 "b": <float>
             }
             where:
@@ -27,7 +28,7 @@ def zero_order_equation(tool_input, cat):
             - y: List of float values for the dependent variable calculated using the zero-order equation (y = a*x + b).
     """
 
-    data = json.loads(tool_input) 
+    data = json.loads(tool_input)
     x = np.array(data["x"])
     a = data["a"]
     b = data["b"]
@@ -36,11 +37,26 @@ def zero_order_equation(tool_input, cat):
     y = a * x + b
 
     # Prepare the output as JSON
-    output = {
-        "x": x.tolist(),
-        "slope": a,
-        "intercept": b,
-        "y": y.tolist()
-    }
+    output = {"x": x.tolist(), "slope": a, "intercept": b, "y": y.tolist()}
 
     return str("Use the following JSON data in your response: " + json.dumps(output))
+
+
+@tool
+def time_to_finish(tool_input, cat) -> str:
+    """
+    Returns the time to the finish of the drying process.
+    
+    Provide an emtpy input
+    The time left in minutes (integer).
+    """
+
+    # for testing all dryers will be done at midnight, so compute the minutes left until midnight
+    import datetime
+
+    now = datetime.datetime.now(datetime.timezone.utc)
+    next_midnight = (now + datetime.timedelta(days=1)).replace(
+        hour=0, minute=0, second=0, microsecond=0
+    )
+    time_left = (next_midnight - now).total_seconds() // 60
+    return str(time_left) # important must be a string other the cat crashes
