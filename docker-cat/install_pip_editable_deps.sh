22ae27395a427f2c3e826f861ae5929f40d57112
Simone Massaro <simone.massaro@mone27.net>
2025-03-04 12:45:53 +0100

    install editable requirements manually

---
+++ docker-cat/install_pip_editable_deps.sh
@@ -0,0 +1,3 @@
+# the cat doesn't allow for editable/local dependencies due to the reliance on https://packaging.pypa.io/en/stable/requirements.html
+# this script will install the dependencies in the editable form
+pip install -e /drymulator-client
