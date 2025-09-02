36ada75b11584760f78513a033313b7b8b1dd0f9
Simone <simone.massaro@mone27.net>
2025-09-02 10:20:03 +0200

    finished refactor in run-weave

--- runs/run-weave-eval.py
+++ runs/run-weave-eval.py
@@ -1,5 +1,4 @@
 from cat_tools.eval import eval_configs, load_eval_dataset
-from cat_tools.setup_declarative_memory import setup_declarative_memory
 import weave
 import asyncio
 import os
@@ -9,8 +8,6 @@ os.environ['VERTEXAI_LOCATION'] = "europe-west4"
 
 weave.init("smart-drying-unitus/declarative-eval")
 
-
-setup_declarative_memory()
 dataset = load_eval_dataset()
 
 asyncio.run(eval_configs(dataset, n_rep=1))
